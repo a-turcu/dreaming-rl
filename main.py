efa100e28ec8cd0ce95305240d79678b8c5072e5
Alexandru Turcu <alexandru.turcu2000@gmail.com>
2022-03-15 19:38:10 +0100

    observation wrapper added

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import gym
+from gym.core import ObservationWrapper
+from gym.spaces import Box
 import matplotlib.pyplot as plt
+import cv2
 
-env = gym.make('ALE/Breakout-v5')
-print(env.action_space.n)
-print(env.observation_space.shape)
+class ObsPreprocess(ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.size = (64, 64)
+        self.observation_space = Box(0.0, 1.0, (self.size[0], self.size[1], 1))
+
+    def observation(self, obs):
+        
+        obs = obs[34:-16, :]
+        
+        obs = cv2.resize(obs, (64, 64))
+        return obs
+
+
+env = gym.make('ALE/Breakout-v5', obs_type = "grayscale", full_action_space=False)#, render_mode="human")
+env = ObsPreprocess(env)
+# 1 useless action?
+# print(env.action_space)
+#print(env.observation_space.shape)
+# print(env.unwrapped.get_action_meanings())
+#help(env.unwrapped)
+
 env.reset()
 for _ in range(50):
     obs, _, _, _ = env.step(env.action_space.sample())
-
-
-plt.title("Game image")
-plt.imshow(env.render("rgb_array"))
-plt.show()
-
-# env.reset()
-# for _ in range(1000):
-#     env.render()
-#     env.step(env.action_space.sample()) # take a random action
-# env.close()
