ebdea76c7c60e2648525581eb30776aafce5086a
Alexandru Turcu <alexandru.turcu2000@gmail.com>
2022-04-20 15:40:21 +0300

    atari localized

--- dqn_job.sh
+++ dqn_job.sh
@@ -8,15 +8,14 @@
 #SBATCH --job-name=VanillaDQN
 
 module load Python/3.7.4-GCCcore-8.3.0 
-module load OpenBLAS/0.3.15-GCC-10.3.0
 module load TensorFlow/2.1.0-fosscuda-2019b-Python-3.7.4
 module load OpenAI-Gym/0.17.1-foss-2019b-Python-3.7.4
 
-source /data/$USER/.envs/first_env/bin/activate
+source /data/$USER/.envs/pyenv37/bin/activate
 
 echo Starting Python program
 
-python /data/s3972445/.envs/first_env/dreaming-rl/main_peregrine.py
+python /data/s3972445/.envs/pyenv37/dreaming-rl/main_peregrine.py
  
 
 
--- main_peregrine.py
+++ main_peregrine.py
@@ -2,7 +2,8 @@
 # QUESTIONS:
 
 
-from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+#from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+from atari_wrappers import make_atari, wrap_deepmind
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
